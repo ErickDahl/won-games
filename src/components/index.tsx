f5e60bc0702348252fa6786712f26ad08b2bf829
Erick <erickdahl1@gmail.com>
2024-12-10 17:58:09 -0300

    fix: fix colors variables

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,7 @@
 const Main = ({ title = 'Hi, Im the main page' }: { title?: string }) => {
   return (
     <main className="bg-primary font-default">
-      <h1 className="bg-primary rounded px-1 py-0.5 text-center">{title}</h1>
+      <h1 className="rounded px-1 py-0.5 text-center">{title}</h1>
     </main>
   )
 }
