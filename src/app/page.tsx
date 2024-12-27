ddb3d3323e96c2f6aac31c3e46d6e786e738d13e
Erick Dahl <erickdahl1@gmail.com>
2024-12-27 19:22:12 -0300

    refactor: remove console log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,6 @@ export default async function Home() {
   const { data } = await supabase.from('games').select('*')
 
   console.log(data)
-  console.log('tes')
 
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 sm:p-20">
