7c39954ec46cdfa0f277fcd244643d6b333ce5e7
Erick <erickdahl1@gmail.com>
2025-01-04 18:04:32 -0300

    chore: update Storybook dependencies and improve configuration for better path resolution

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,30 @@
+import { dirname, join } from 'path'
 import type { StorybookConfig } from '@storybook/nextjs'
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+
   addons: [
-    '@storybook/addon-onboarding',
-    '@storybook/addon-essentials',
-    '@chromatic-com/storybook',
-    '@storybook/addon-interactions'
+    getAbsolutePath('@storybook/addon-onboarding'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/addon-interactions'),
+    '@chromatic-com/storybook'
   ],
+
   framework: {
-    name: '@storybook/nextjs',
+    name: getAbsolutePath('@storybook/nextjs'),
     options: {}
   },
-  staticDirs: ['..\\public']
+
+  staticDirs: ['..\\public'],
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 }
 export default config
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, 'package.json')))
+}
