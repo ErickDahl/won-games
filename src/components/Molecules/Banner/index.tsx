66ac41d894df6221c14910c76e61fd44849c02c0
Erick <erickdahl1@gmail.com>
2025-01-06 17:18:34 -0300

    fix: reorder class names in Banner component for proper styling

--- src/components/Molecules/Banner/index.tsx
+++ src/components/Molecules/Banner/index.tsx
@@ -9,7 +9,7 @@ const bannerClasses = tv({
     base: 'relative flex w-full flex-col',
     image: 'rounded-md',
     containerInformation:
-      'lg:bg-transparentBlack bottom-0 left-0 w-full bg-darkGray p-5 lg:absolute lg:rounded-b-md',
+      'bottom-0 left-0 w-full bg-darkGray p-5 lg:absolute lg:rounded-b-md lg:bg-transparentBlack',
     containerTitles: 'mb-3',
     titleClassName: 'text-xl font-semibold text-white',
     subtitleClassName: 'text-sm font-normal text-white',
